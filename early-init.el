3d5cb201145b3f4294d691fc00877d93b5492245
Daw-Ran Liou <hi@dawranliou.com>
2021-08-24 13:46:36 -0700

    Update early-init

--- early-init.el
+++ early-init.el
@@ -1,7 +1,23 @@
+;;; early-init.el --- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
 (setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
-(setq package-enable-at-startup nil)
-(setq frame-inhibit-implied-resize t)
-;; (push '(menu-bar-lines . 0) default-frame-alist)
-;; (push '(tool-bar-lines . 0) default-frame-alist)
-;; (push '(vertical-scroll-bars . nil) default-frame-alist)
+      gc-cons-percentage 0.6
+      package-enable-at-startup nil
+      frame-inhibit-implied-resize t)
+
+(setq-default default-frame-alist
+              '(;;(menu-bar-lines         . 0)
+                ;;(tool-bar-lines         . 0)
+                ;;(vertical-scroll-bars   . nil)
+                (horizontal-scroll-barr . nil)
+                (width                  . 90)
+                (height                 . 40)
+                (font                   . "Inconsolata-16")))
+
+(provide 'early-init)
+
+;;; early-init.el ends here
